589a2360819bcf6deb852b039eb6c1cd4f006719
GamalShine <119243187+GamalShine@users.noreply.github.com>
2023-09-21 18:40:17 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,4 @@
+<?php
+$connect = mysqli_connect("localhost","root","","login") or die ("Gagal Terhubung Ke Database");
+// $link = mysqli_select_db("login") or die ("Database Tidak Ditemukan");
+?>
